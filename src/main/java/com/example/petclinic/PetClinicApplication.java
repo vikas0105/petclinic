81bfa37ccafd5fec6dca663c36f61eff3e6d44be
vikas <gowdavikas0105@gmail.com>
2024-12-21 19:36:31 +0530

    test

--- src/main/java/com/example/petclinic/PetClinicApplication.java
+++ src/main/java/com/example/petclinic/PetClinicApplication.java
@@ -3,7 +3,7 @@ package com.example.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.example.petclinic.repository") // Ensures Spring scans for repositories
